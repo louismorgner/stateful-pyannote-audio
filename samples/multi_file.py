bb947dc7a90841ec5d6ee057960599a3b9cc13c3
Louis Morgner <62328448+louismorgner@users.noreply.github.com>
2024-08-22 11:06:25 +0200

    got it to work locally

---
+++ samples/multi_file.py
@@ -0,0 +1,11 @@
+from pyannote.audio import Pipeline
+pipeline = Pipeline.from_pretrained(
+    "pyannote/speaker-diarization-3.1",
+    use_auth_token="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")
+
+# apply pretrained pipeline
+diarization = pipeline("./chunk1.wav")
+
+# print the result
+for turn, _, speaker in diarization.itertracks(yield_label=True):
+    print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
